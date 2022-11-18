7632445b7d212cf113c19ee9dafe832b502b3670
Miguel Mota <hello@miguelmota.com>
2022-11-18 00:24:52 -0800

    Add proof node viz

--- merkletreeviz.js
+++ merkletreeviz.js
@@ -2194,26 +2194,50 @@ class MerkleTreeViz {
     return hash.slice(0, 4) + '…' + hash.slice(hash.length - 4, hash.length)
   }
 
-  construct (key, layers, idx, depth) {
+  construct (key, layers, idx, depth, proof) {
+    let className = 'parent-node'
+    let isProof = proof && proof.includes('0x' + key)
+    const isRoot = idx === 0
+    if (isProof) {
+      if (isRoot) {
+        className = 'proof-root-node'
+      } else {
+        className = 'proof-parent-node'
+      }
+    } else if (isRoot) {
+      className = 'root-node'
+    }
     const obj = {
       text: {
         name: this.getLabel(key)
       },
-      HTMLclass: idx === 0 ? 'root-node' : 'parent-node'
+      HTMLclass: className
     }
     idx++
+    const isLeaf = idx === depth
     if (layers[key]) {
       obj.children = []
       let i = 0
       for (const k in layers[key]) {
+        isProof = proof && proof.includes('0x' + k)
+        className = 'parent-node'
+        if (isProof) {
+          if (isLeaf) {
+            className = 'proof-leaf-node'
+          } else {
+            className = 'proof-parent-node'
+          }
+        } else if (isLeaf) {
+          className = 'leaf-node'
+        }
         obj.children.push({
           text: {
             name: this.getLabel(k)
           },
-          HTMLclass: idx === depth ? 'leaf-node' : 'parent-node'
+          HTMLclass: className
         })
         if (layers[key][k]) {
-          obj.children[i] = this.construct(k, layers[key], idx, depth)
+          obj.children[i] = this.construct(k, layers[key], idx, depth, proof)
         }
         i++
       }
@@ -2222,50 +2246,18 @@ class MerkleTreeViz {
   }
 
   renderTree (merkleTree) {
-    const styles = document.createElement('style')
-    styles.id = 'merkletreeviz-css'
-    styles.innerText = `
-      /* required lib styles */
-      .Treant { position: relative; overflow: hidden; padding: 0 !important; }
-      .Treant > .node,
-      .Treant > .pseudo { position: absolute; display: block; visibility: hidden; }
-      .Treant.Treant-loaded .node,
-      .Treant.Treant-loaded .pseudo { visibility: visible; }
-      .Treant > .pseudo { width: 0; height: 0; border: none; padding: 0; }
-      .Treant .collapse-switch { width: 3px; height: 3px; display: block; border: 1px solid black; position: absolute; top: 1px; right: 1px; cursor: pointer; }
-      .Treant .collapsed .collapse-switch { background-color: #868DEE; }
-      .Treant > .node img {	border: none; float: left; }
-
-      .node {
-        border: 1px solid #000;
-        padding: 0 1rem;
-        font-family: monospace;
-      }
-
-      .node.root-node {
-        background-color: #d4e7d3;
-      }
-      .node.parent-node {
-        background-color: #dae8fc;
-      }
-      .node.leaf-node {
-        background-color: #fee7d0;
-      }
-    `
-    if (!document.querySelector('#merkletreeviz-css')) {
-      document.body.appendChild(styles)
-    }
-
+    this._insertStyles()
+    this.tree = merkleTree
     const layers = merkleTree.getLayersAsObject()
     const depth = merkleTree.getDepth()
     return this.renderLayersAsObject(layers, depth)
   }
 
-  renderLayersAsObject (layers, depth) {
+  renderLayersAsObject (layers, depth, proof) {
     let nodeTreeStructure = {}
     const idx = 0
     for (const key in layers) {
-      nodeTreeStructure = this.construct(key, layers, idx, depth)
+      nodeTreeStructure = this.construct(key, layers, idx, depth, proof)
     }
 
     const chartConfig = {
@@ -2286,10 +2278,69 @@ class MerkleTreeViz {
     return viz
   }
 
-  destroy() {
+  destroy () {
     if (this.instance) {
       this.instance.destroy()
     }
+  }
+
+  _insertStyles () {
+    const styles = document.createElement('style')
+    styles.id = 'merkletreeviz-css'
+    styles.innerText = `
+      /* required lib styles */
+      .Treant { position: relative; overflow: hidden; padding: 0 !important; }
+      .Treant > .node,
+      .Treant > .pseudo { position: absolute; display: block; visibility: hidden; }
+      .Treant.Treant-loaded .node,
+      .Treant.Treant-loaded .pseudo { visibility: visible; }
+      .Treant > .pseudo { width: 0; height: 0; border: none; padding: 0; }
+      .Treant .collapse-switch { width: 3px; height: 3px; display: block; border: 1px solid black; position: absolute; top: 1px; right: 1px; cursor: pointer; }
+      .Treant .collapsed .collapse-switch { background-color: #868DEE; }
+      .Treant > .node img {  border: none; float: left; }
+
+      .node {
+        border: 1px solid #000;
+        padding: 0 1rem;
+        font-family: monospace;
+      }
+      .node.root-node {
+        background-color: #d4e7d3;
+      }
+      .node.proof-root-node {
+        background-color: #d4e7d3;
+        outline: 5px solid #FFFF00;
+      }
+      .node.parent-node {
+        background-color: #dae8fc;
+      }
+      .node.proof-parent-node {
+        background-color: #dae8fc;
+        outline: 5px solid #FFFF00;
+      }
+      .node.leaf-node {
+        background-color: #fee7d0;
+      }
+      .node.proof-leaf-node {
+        background-color: #fee7d0;
+        outline: 5px solid #FFFF00;
+      }
+    `
+    if (!document.querySelector('#merkletreeviz-css')) {
+      document.body.appendChild(styles)
+    }
+  }
+
+  renderProof (proof) {
+    if (!this.tree) {
+      return
+    }
+
+    this._insertStyles()
+    this.destroy()
+    const layers = this.tree.getLayersAsObject()
+    const depth = this.tree.getDepth()
+    return this.renderLayersAsObject(layers, depth, proof)
   }
 }
 
